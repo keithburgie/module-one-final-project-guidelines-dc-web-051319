89fa8f684a4f7d89c491f469258291250111c340
Natany Caxeta <natanycaxeta@icloud.com>
2019-05-28 15:11:02 -0400

    created the owner and pet_owner tables

--- db/migrate/20190528184918_create_owners_table.rb
+++ db/migrate/20190528184918_create_owners_table.rb
@@ -1,4 +1,9 @@
 class CreateOwnersTable < ActiveRecord::Migration[5.0]
   def change
+    create_table :owner do |t|
+        t.string :name
+        t.integer :zip_code
+        t.string :kind
+      end
   end
 end
--- db/migrate/20190528185138_create_pet_owners_table.rb
+++ db/migrate/20190528185138_create_pet_owners_table.rb
@@ -1,4 +1,9 @@
 class CreatePetOwnersTable < ActiveRecord::Migration[5.0]
   def change
+    create_table :pet_owners do |t|
+      t.integer :pet_id
+      t.integer :owner_id
+      t.boolean :current?
+    end
   end
 end
