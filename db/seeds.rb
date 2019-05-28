506327d06291e9c0cc0e796cfd4f5b7da93f423f
Natany Caxeta <natanycaxeta@icloud.com>
2019-05-28 16:59:30 -0400

    changed the pet size XX-large to large

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,7 +23,7 @@ owner3 = Owner.find_or_create_by({name: "Animal Shelter", zip_code: 20002, kind:
 
 pet1 = Pet.find_or_create_by({name: "Bob", breed: "mutt", age: "3", status: "ready for adoption", color: "black", size: "medium", fee: "50", bio: "Lorem ipsum dolor sit amet.", gender: "male", species: "canine"})
 pet2 = Pet.find_or_create_by({name: "Ruby", breed: "persa", age: "10", status: "ready for adoption", color: "brown", size: "medium", fee: "50", bio: "Lorem ipsum dolor sit amet.", gender: "female", species: "feline"})
-pet3 = Pet.find_or_create_by({name: "Soda", breed: "old english sheepdog", age: "5", status: "ready for adoption", color: "gray", size: "xx-large", fee: "50", bio: "Lorem ipsum dolor sit amet.", gender: "female", species: "canine"})
+pet3 = Pet.find_or_create_by({name: "Soda", breed: "old english sheepdog", age: "5", status: "ready for adoption", color: "gray", size: "large", fee: "50", bio: "Lorem ipsum dolor sit amet.", gender: "female", species: "canine"})
 pet4 = Pet.find_or_create_by({name: "Coke", breed: "sphynx", age: "1", status: "ready for adoption", color: "gray", size: "small", fee: "50", bio: "Lorem ipsum dolor sit amet.", gender: "male", species: "feline"})
 pet5 = Pet.find_or_create_by({name: "Pepsi", breed: "dachshund", age: "1", status: "ready for adoption", color: "black", size: "small", fee: "50", bio: "Lorem ipsum dolor sit amet.", gender: "male", species: "canine"})
 pet6 = Pet.find_or_create_by({name: "Cheese", breed: "dachshund", age: "1", status: "ready for adoption", color: "black", size: "small", fee: "50", bio: "Lorem ipsum dolor sit amet.", gender: "male", species: "canine"})
