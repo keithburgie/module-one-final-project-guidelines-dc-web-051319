a3eb5e719fa1d6c0c78ae0b9236a91e3b453daf5
keithburgie <krburgie@gmail.com>
2019-05-29 11:29:49 -0400

    Building new adoptees

--- bin/run.rb
+++ bin/run.rb
@@ -16,22 +16,28 @@ def reason_for_visit
     greeting = "How can we help you today?"
     choices = ["I'm here to adopt.", "I have an animal to surrender."]
     reason = prompt.select(greeting, choices)
-    if reason == choices[0]
-        adopt_pets
-    elsif reason == choices[1]
-        surrender_pets
+    if reason == choices[0] #adopt
+        here_to_adopt_pet
+    elsif reason == choices[1] #surrender
+        here_to_surrender_pet
     end
 end
 
-def surrender_pets
+def here_to_surrender_pet
     puts "Sorry, we're full!"
     #reason_for_visit
 end
 
-def adopt_pets
+def here_to_adopt_pet
     greet_adoptee
-    email_address = collect_adoptee_info
+    email_address = gets.chomp
     adoptee = Owner.find_or_create_by({email: email_address, kind: "Person"})
+    #binding.pry
+    if adoptee.name
+        greet_known_adoptee(adoptee)
+    else
+        build_new_adoptee(adoptee)
+    end
 end
 
 def greet_adoptee
@@ -42,4 +48,15 @@ def collect_adoptee_info
     gets.chomp
 end
 
-adopt_pets
+def build_new_adoptee(adoptee)
+    puts "Hey, you're new here! What's your name?"
+    adoptee.name = gets.chomp
+    puts "What's your zip code?"
+    adoptee.zip_code = gets.chomp
+end
+
+def greet_known_adoptee(adoptee)
+    puts "Hello, #{adoptee.name}! Good to see you again."
+end
+
+here_to_adopt_pet
