f2efc0e512a0f55517e9d2d66406010c4365127f
keithburgie <krburgie@gmail.com>
2019-05-30 12:58:20 -0400

    Working backup

--- bin/run.rb
+++ bin/run.rb
@@ -2,10 +2,12 @@ require_relative '../config/environment'
 require 'tty-prompt'
 require'pry'
 
+
 def welcome
 	this_shelter = Owner.all.find_by(kind: "Shelter")
 	puts "Welcome to #{this_shelter.name}"
 end
+
 
 def reason_for_visit?
 	prompt = TTY::Prompt.new
@@ -15,6 +17,7 @@ def reason_for_visit?
 		end
 		reason == "adopt" ? here_to_adopt_pet : here_to_surrender_pet
 end
+
 
 def here_to_surrender_pet
 	prompt = TTY::Prompt.new
@@ -26,6 +29,7 @@ def here_to_surrender_pet
 	end
 end
 
+
 def here_to_adopt_pet
 	prompt = TTY::Prompt.new
 		email_address = prompt.ask('"Great! First, what is your email?') { |q| q.validate :email }
@@ -33,6 +37,7 @@ def here_to_adopt_pet
 	find_or_create_adoptee(adoptee)
 	adoption_type
 end
+
 
 def find_or_create_adoptee(adoptee)
     if adoptee.name #Adoptee is known if they have a name
@@ -42,15 +47,18 @@ def find_or_create_adoptee(adoptee)
     end
 end
 
+
 def build_new_adoptee(adoptee)
 	name = prompt.ask("Hey, you're new here! What's your name?", required: true)
 	adoptee.name = name
 	adoptee.save
 end
 
+
 def greet_known_adoptee(adoptee)
 	puts "Hi, #{adoptee.name}! Good to see you again."
 end
+
 
 def adoption_type
   prompt = TTY::Prompt.new
@@ -62,67 +70,72 @@ def adoption_type
 	end
 
 	pets = []
-
-	get_animals(animal_search).each_with_index do |pet_owner, index| 
+	get_animals(animal_search).each_with_index do |pet_owner, index|
 		pets << list_animals(pet_owner, index)
 	end
 
+	#show all pets from search
 	animal_select(pets)
 end
 
-def list_animals(pet_owner, index)
-    # Choose a pet to see bio and have the option to adopt
-    # Have an option to go back to list
-    emoji = ""
-			if pet_owner.pet.species == "feline"
-					emoji = "🐱"
-			elsif pet_owner.pet.species == "canine"
-					emoji = "🐶"
-			elsif pet_owner.pet.species == "n. hollandicus"
-					emoji = "🐥"
-    end
-    #"#{pet_owner.pet.species.capitalize} ##{index+1}: #{pet_owner.pet.name}, a #{pet_owner.pet.color} #{pet_owner.pet.age}-year-old #{pet_owner.pet.gender} #{pet_owner.pet.breed.capitalize}"
-    #"#{emoji} #{pet_owner.pet.name.capitalize}, a #{pet_owner.pet.color} #{pet_owner.pet.age}-year-old #{pet_owner.pet.gender} #{pet_owner.pet.breed.capitalize}"
-		pet_owner.pet
-	end
 
 def animal_select(pets)
-    prompt = TTY::Prompt.new
-    header = "Available Pets:"
-    pet_selection = prompt.select(header, pets)
+	prompt = TTY::Prompt.new
+	header = "Available Pets:"
+	pet_selection = prompt.select(header, pets)
 end
+
+
+def list_animals(pet_owner, index)
+	# Choose a pet to see bio and have the option to adopt
+		# Have an option to go back to list
+		# emoji = ""
+		# 	if pet_owner.pet.species == "feline"
+		# 			emoji = "🐱"
+		# 	elsif pet_owner.pet.species == "canine"
+		# 			emoji = "🐶"
+		# 	elsif pet_owner.pet.species == "n. hollandicus"
+		# 			emoji = "🐥"
+		# end
+		#"#{pet_owner.pet.species.capitalize} ##{index+1}: #{pet_owner.pet.name}, a #{pet_owner.pet.color} #{pet_owner.pet.age}-year-old #{pet_owner.pet.gender} #{pet_owner.pet.breed.capitalize}"
+	#"#{emoji} #{pet_owner.pet.name.capitalize}, a #{pet_owner.pet.color} #{pet_owner.pet.age}-year-old #{pet_owner.pet.gender} #{pet_owner.pet.breed.capitalize}"
+	pet_owner.pet
+end
+
 
 def shelter_animals #checking if the animal have the shelter as owner
-    pets_shelter = PetOwner.all.select do |pet_owner|
-        if pet_owner.owner.kind == "Shelter"
-            pet_owner
-        end
-    end
+	pets_shelter = PetOwner.all.select do |pet_owner|
+		if pet_owner.owner.kind == "Shelter"
+			pet_owner
+		end
+	end
 end
 
+
 def get_animals(pet_species)
-    if pet_species == "feline"
-        shelter_animals.select do |pet_owner|
-            #show felines
-            pet_owner.pet.species == "feline"
-        end
-    elsif pet_species == "canine"
-        shelter_animals.select do |pet_owner|
-            #show canines
-            pet_owner.pet.species == "canine"
-        end
-    elsif pet_species == "other"
-        shelter_animals.select do |pet_owner|
-            #show pets besides felines or canines
-            pet_owner.pet.species != "feline" && pet_owner.pet.species != "canine"
-        end
-    else
-        shelter_animals.select do |pet_owner|
-            #show all available pets
-            pet_owner.pet
-        end
-    end
+	if pet_species == "feline"
+		shelter_animals.select do |pet_owner|
+			#show felines
+			pet_owner.pet.species == "feline"
+		end
+	elsif pet_species == "canine"
+		shelter_animals.select do |pet_owner|
+			#show canines
+			pet_owner.pet.species == "canine"
+		end
+	elsif pet_species == "other"
+		shelter_animals.select do |pet_owner|
+			#show pets besides felines or canines
+			pet_owner.pet.species != "feline" && pet_owner.pet.species != "canine"
+		end
+	else
+		shelter_animals.select do |pet_owner|
+			#show all available pets
+			pet_owner.pet
+		end
+	end
 end
+
 
 def runner
     welcome
